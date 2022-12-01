ae1f0a4dcdde7afb963dd8d2e13045bea48fcb6c
katriosa <ee.chistyakova@gmail.com>
2022-12-01 13:43:02 +0400

    feat: add rotate dice

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-const diceEl = document.querySelector(".dice");
+const diceEl = document.querySelector(".dice-img");
 const currentScore0 = document.querySelector(".current--0");
 const currentScore1 = document.querySelector(".current--1");
 const score0 = document.querySelector(".score--0");
 const score1 = document.querySelector(".score--1");
 const player0 = document.querySelector(".player--0");
 const player1 = document.querySelector(".player--1");
+const containerDice = document.querySelector(".container");
 
-// playerIndex = [0, 1];
-// let activePlayer = `player${playerIndex[0]}`;
-// console.log(activePlayer);
+////////////////////////////////////////////////////////////////////////////
+//rotate  dice
+const cube = document.querySelector(".cube");
+const front = document.querySelector(".front");
+const getRandom = function (min = 1, max = 6) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let sides = {
+  side1: [0, 0],
+  side2: [180, 0],
+  side3: [270, 0],
+  side4: [90, 0],
+  side5: [0, 270],
+  side6: [0, 90],
+};
+let startSide = [0, 0];
+
+////////////////////////////////////////////////////////////////////////////
 let activePlayer = 0;
 let currentScoreActiv;
 let scoreActiv;
@@ -21,7 +39,7 @@ const init = function () {
   currentScore1.textContent = 0;
   score0.textContent = 0;
   score1.textContent = 0;
-  diceEl.classList.add("hidden");
+  containerDice.classList.add("hidden");
 };
 init();
 
@@ -37,13 +55,6 @@ const switchPlayer = function () {
 //new game
 btnNew.addEventListener("click", init);
 
-// //roll dice
-// const getRandom = function (min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
-
 const showResultCurrent = function (randomNumber) {
   currentScoreActiv = document.querySelector(`.current--${activePlayer}`);
   if (randomNumber === 1) {
@@ -56,11 +67,33 @@ const showResultCurrent = function (randomNumber) {
 };
 
 btnRoll.addEventListener("click", function () {
-  diceEl.classList.remove("hidden");
-  let randomNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-  diceEl.setAttribute("src", `dice-${randomNumber}.png`);
+  containerDice.classList.remove("hidden");
+  let randomNumber = getRandom();
+  // diceEl.setAttribute("src", `dice-${randomNumber}.png`);
+  //rotate dice
 
-  showResultCurrent(randomNumber);
+  let randomSide = sides[`side${randomNumber}`];
+  cube.animate(
+    [
+      // keyframes
+      { transform: `rotateY(${startSide[0]}deg) rotateX(${startSide[1]}deg)` },
+
+      {
+        transform: `rotateY(${randomSide[0] + 360}deg) rotateX(${
+          randomSide[1] + 360
+        }deg)`,
+      },
+    ],
+    {
+      // timing options
+      duration: 1000,
+    }
+  );
+  cube.style.transform = `rotateY(${randomSide[0]}deg) rotateX(${randomSide[1]}deg)`;
+
+  window.setTimeout(function () {
+    showResultCurrent(randomNumber);
+  }, 2000);
 });
 
 //hold
